f29307414673a76427a706e2a50b8702811cee13
Logon Aluno <logonaluno@local.com>
2020-03-05 21:48:22 -0300

    primeiro commit

---
+++ betterdriver/src/main/java/com/betterdriver/BetterdriverApplication.java
@@ -0,0 +1,13 @@
+package com.betterdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterdriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterdriverApplication.class, args);
+	}
+
+}
